2519d489b3f49b8d08205c213c6b23723bfeff63
Chiu Hoi Ping <screw123@gmail.com>
2020-03-03 21:34:09 +0800

    update filename

--- index.mjs
+++ index.mjs
@@ -2,6 +2,65 @@ import express from 'express'
 
 const app = express()
 
-app.use('/hello', (req, res)=>res.send('OK'))
+app.use('/hello', (req, res, next)=>res.send('OK'))
 
 app.listen(3000)
+
+
+
+
+import XXX
+
+const name = "Kris"
+const phone as String = "12345678"
+const age as Integer = 30
+
+return phone + age
+
+
+
+String
+------
+"abc", "123"
+
+Integer
+--------
+1, 2, 3, 0, 38472384
+
+Float
+------
+1.3, 2.6, 3.9+e10
+
+Boolean
+-------
+true, false
+
+const isMingAMan = true
+
+if (isMingAMan == true) {
+    1
+}
+else {
+    2
+}
+
+Array
+const a = [3, "a", true, 1.5, "bavdafsg", 3, 3]
+          [0,  1,    2,    3,      4,     5, 6]
+
+console.log(a[1])  //"a"
+console.log(a[0]) //3
+console.log(a[6]) //3
+console.log(a.length)   //7
+
+Object
+
+
+
+
+
+const app = express()
+
+const f = (a, b, c) => {
+    return a
+}
